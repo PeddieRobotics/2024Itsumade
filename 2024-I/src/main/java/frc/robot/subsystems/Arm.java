38bc95e8e1b3529c1199f8af6eac6bd6fda09eb8
Megan Cheng <mcheng-27@peddie.org>
2024-01-11 17:11:37 -0500

    Completed Skeleton Code

---
+++ 2024-I/src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems;
+
+public class Arm {
+    public static Arm instance;
+    public enum ArmState {Intaking, Moving, Stowed, Shooting};
+
+    private ArmState state, goalState;
+
+    public static Arm getInstance(){
+        if (instance == null){
+            instance = new Arm();
+        }
+        return instance;
+    }
+
+    public Arm(){
+        state = ArmState.Stowed;
+        goalState = ArmState.Stowed;
+    }
+
+    public void RequestState(ArmState requestedState){
+        if (requestedState == state || requestedState == goalState){
+            return;
+        }
+        state = ArmState.Moving; 
+        goalState = requestedState;
+    }
+
+    public void periodic(){
+
+    }
+
+}
---
+++ 2024-I/src/main/java/frc/robot/util/Constants.java
@@ -0,0 +1,30 @@
+package frc.robot.util;
+
+public final class Constants {
+    public static class GlobalConstants{
+        public static final boolean useLEDLights = true;
+    }
+
+    public static class OIConstants{
+        public static final boolean useDebugModeLayout = true;
+        public static final double drivingDeadband = 0.1;
+    }
+
+    public static class DriveConstants{
+        
+    }
+
+    public static class ModuleConstants{
+        
+    }
+
+    public static class AutoConstants{
+        
+    }
+
+    public static class LimelightConstants{
+        public static final double limelightHeight = 0;
+        public static final double limelightPanningAngle = 0;
+        public static final double limelightAngle = 0;
+    }
+}
