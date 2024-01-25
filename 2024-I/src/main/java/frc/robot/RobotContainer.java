f938fd91ecc24067e5ca0b06143b6f2f71947772
kaseyLee123 <klee-27@peddie.org>
2024-01-25 17:30:55 -0500

    swerve base code (2 module)

--- 2024-I/src/main/java/frc/robot/RobotContainer.java
+++ 2024-I/src/main/java/frc/robot/RobotContainer.java
@@ -5,16 +5,16 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.Arm;
-import frc.robot.subsystems.Autonomous;
-import frc.robot.subsystems.Climber;
+import frc.robot.commands.DriveCommands.SwerveDriveCommand;
+import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Octopus;
 
 
 public class RobotContainer {
   // private final Climber climber; 
   // private final Arm arm; 
-  private final Octopus bubbles;
+  // private final Octopus bubbles;
+  private final Drivetrain drivetrain;
   // private final Autonomous autonomous;
 
   public Command getAutonomousCommand() {
@@ -26,7 +26,10 @@ public class RobotContainer {
   public RobotContainer(){
     // arm = Arm.getInstance();
     // climber = Climber.getInstance();
-    bubbles = Octopus.getInstance();
+    drivetrain = Drivetrain.getInstance();
+    // bubbles = Octopus.getInstance();
     // autonomous = Autonomous.getInstance();
+
+    drivetrain.setDefaultCommand(new SwerveDriveCommand());
   }
 }
