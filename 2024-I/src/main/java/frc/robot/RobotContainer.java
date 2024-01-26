0a0ac87bfa4d348369a0053dcd0149b6ffc04604
philiphan0109 <51898400+philiphan0109@users.noreply.github.com>
2024-01-26 16:14:29 -0500

    moneky

--- 2024-I/src/main/java/frc/robot/RobotContainer.java
+++ 2024-I/src/main/java/frc/robot/RobotContainer.java
@@ -5,13 +5,10 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.DriveCommands.SwerveDriveCommand;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.Octopus;
-
 
 public class RobotContainer {
   // private final Climber climber; 
   // private final Arm arm; 
-  // private final Octopus bubbles;
   private final Drivetrain drivetrain;
   // private final Autonomous autonomous;
 
@@ -25,7 +22,6 @@ public class RobotContainer {
     // arm = Arm.getInstance();
     // climber = Climber.getInstance();
     drivetrain = Drivetrain.getInstance();
-    // bubbles = Octopus.getInstance();
     // autonomous = Autonomous.getInstance();
 
     drivetrain.setDefaultCommand(new SwerveDriveCommand());
