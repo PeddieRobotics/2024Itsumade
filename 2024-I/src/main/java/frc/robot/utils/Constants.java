66688cf4f9268224565385f129aa58b714be82ed
SeanLi1234 <sli-25@peddie.org>
2024-02-03 13:14:54 -0500

    Revert "thing"
    This reverts commit c64089e57e427e7d5245f426482fd3f68c34f3b8.

--- 2024-I/src/main/java/frc/robot/utils/Constants.java
+++ 2024-I/src/main/java/frc/robot/utils/Constants.java
@@ -17,16 +17,16 @@ public final class Constants {
 
   public static class DriveConstants {
     // TODO: Update these constants
-    public static final double kTrackWidth = Units.inchesToMeters(0);
-    public static final double kWheelBase = Units.inchesToMeters(0);
+    public static final double kTrackWidth = Units.inchesToMeters(20);
+    public static final double kWheelBase = Units.inchesToMeters(20);
 
     public static final double kBaseRadius = Math.sqrt(Math.pow(kTrackWidth, 2) + Math.pow(kWheelBase, 2)) / 2;
 
     public static final Translation2d[] swerveModuleLocations = {
+        new Translation2d(-kWheelBase / 2.0, kTrackWidth / 2.0),
         new Translation2d(kWheelBase / 2.0, kTrackWidth / 2.0),
-        new Translation2d(kWheelBase / 2.0, kTrackWidth / 2.0),
-        new Translation2d(kWheelBase / 2.0, kTrackWidth / 2.0),
-        new Translation2d(kWheelBase / 2.0, kTrackWidth / 2.0)
+        new Translation2d(-kWheelBase / 2.0, -kTrackWidth / 2.0),
+        new Translation2d(kWheelBase / 2.0, -kTrackWidth / 2.0)
     };
 
     public static final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
@@ -42,34 +42,34 @@ public final class Constants {
     public static final int kDriveCurrentLimit = 30;
     public static final int kTurningCurrentLimit = 30;
 
-    public static final double kDrivingMotorGearRatio = 0;
     public static final double kWheelRadius = 2.0;
   }
 
   public static class ModuleConstants {
     public static final double WHEEL_DIAMETER_IN = 4.0;
 
-
     public static final double kDriveMotorReduction = 6.12;
-    public static final double kDrivingEncoderPostionFactor = (Units.inchesToMeters(WHEEL_DIAMETER_IN) * Math.PI)/kDriveMotorReduction;
-    public static final double kDrivingEncoderVelocityFactor = (Units.inchesToMeters(WHEEL_DIAMETER_IN) * Math.PI)/kDriveMotorReduction/60;
+    public static final double kDrivingEncoderPostionFactor = (Units.inchesToMeters(WHEEL_DIAMETER_IN) * Math.PI)
+        / kDriveMotorReduction;
+    public static final double kDrivingEncoderVelocityFactor = (Units.inchesToMeters(WHEEL_DIAMETER_IN) * Math.PI
+        / kDriveMotorReduction);
 
     public static final double kTurningMotorReduction = 150.0 / 7.0;
-    public static final double kTurningEncoderPositonFactor = 2 * Math.PI  / kTurningMotorReduction;
-    public static final double kTurningEncoderVelocityFactor = (2 * Math.PI)/60;
+    public static final double kTurningEncoderPositonFactor = kTurningMotorReduction / (2 * Math.PI);
+    public static final double kTurningEncoderVelocityFactor = (2 * Math.PI) / 60;
 
     public static final double kDrivingMotorCurrentLimit = 50;
     public static final double kTurningMotorCurrentLimit = 50;
 
-    public static final double kDrivingS = 0.0;
-    public static final double kDrivingV = 0.0;
+    public static final double kDrivingS = 0.05;
+    public static final double kDrivingV = 0.13;
     public static final double kDrivingA = 0.0;
-    public static final double kDrivingP = 0.0;
+    public static final double kDrivingP = 0.11;
     public static final double kDrivingI = 0.0;
     public static final double kDrivingD = 0.0;
     public static final double kDrivingFF = 0.0;
 
-    public static final double kTurningP = 2.3;
+    public static final double kTurningP = 36;
     public static final double kTurningI = 0.0;
     public static final double kTurningD = 0.0;
     public static final double kTurningFF = 0.0;
@@ -80,45 +80,38 @@ public final class Constants {
   }
 
   public static class LimelightConstants {
-    public static final double kLimelightHeight = 0;
-    public static final double kLimelightPanningAngle = 0;
-    public static final double kLimelightAngle = 0;
-
+    public static final double limelightHeight = 0;
+    public static final double limelightPanningAngle = 0;
+    public static final double limelightAngle = 0;
   }
 
   public static class ArmConstants {
-    public static final int ARM_PRIMARY_MOTOR_LIMIT = 0;
-    public static final int ARM_SECONDARY_MOTOR_LIMIT = 0;
-    public static final int ARM_P = 0;
-    public static final int ARM_I = 0;
-    public static final int ARM_D = 0;
-    public static final int ARM_FF = 0;
-    public static final int ARM_IZONE = 0;
+    public static final int kArmPrimaryCurrentLimit = 0;
+    public static final int kArmSecondaryCurrentLimit = 0;
+    public static final double kArmP = 0;
+    public static final double kArmI = 0;
+    public static final double kArmD = 0;
+    public static final double kArmFF = 0;
+    public static final double kArmIZone = 0;
   }
 
   public static class FlywheelConstants {
-    public static final int FLYWHEEL_PRIMARY_MOTOR_LIMIT = 0;
-    public static final int FLYWHEEL_SECONDARY_MOTOR_LIMIT = 0;
+    public static final int kFlywheelLeftCurrentLimit = 0;
+    public static final int kFlywheelRightCurrentLimit = 0;
+    
+    public static final double kFlywheelS = 0;
+    public static final double kFlywheelV = 0;
+    public static final double kFlywheelA = 0;
+    public static final double kFlywheelP = 0;
+    public static final double kFlywheelI = 0;
+    public static final double kFlywheelD = 0;
+    public static final double kFlywheelFF = 0;
   }
 
   public static class IntakeConstants {
-    public static final int INTAKE_MOTOR_LIMIT = 0;
-    public static final int HOPPER_MOTOR_LIIMT = 0;
+    public static final int kIntakeCurrentLimit = 0;
+    public static final int kHopperCurrentLimit = 0;
+    public static final double kIntakeSensorThreshold = 0;
+    public static final double kHopperSensorThreshold = 0;
   }
-
-  public static class PIDConstants{
-    public static final double VELOCITY_S = 0.05;
-    public static final double VELOCITY_V = 0.12;
-    public static final double VELOCITY_P = 0.08;
-    public static final double VELOCITY_I = 0;
-    public static final double VELOCITY_D = 0;
-    public static final double VELOCITY_FF = 0;
-
-    public static final double POSITION_P = 0;
-    public static final double POSITION_I = 0;
-    public static final double POSITION_D = 0;
-    public static final double POSITION_FF = 0;
-  }
-
-  
 }
