8f1bfc0402af27716dfceef46dcdc667b752acb0
philiphan0109 <51898400+philiphan0109@users.noreply.github.com>
2024-02-27 15:30:52 -0500

    p

--- 2024-I/src/main/java/frc/robot/utils/Constants.java
+++ 2024-I/src/main/java/frc/robot/utils/Constants.java
@@ -51,10 +51,10 @@ public final class Constants {
     //1. remove offsets (set all to 0) and then deploy code
     //2. spin all modules so that bevel gears face left relative to robot (shooter in front)
     //3. read the cancoder values from dashboard, and put those values for these offsets (check robotmap for ids)
-    public static final double kFrontLeftModuleAngularOffset = -2.844;
-    public static final double kFrontRightModuleAngularOffset = 1.316;
-    public static final double kBackLeftModuleAngularOffset = -2.083;
-    public static final double kBackRightModulelAngularOffset = 2.155;
+    public static final double kFrontLeftModuleAngularOffset = -2.837864;
+    public static final double kFrontRightModuleAngularOffset = 0.579844;
+    public static final double kBackLeftModuleAngularOffset = -2.078543;
+    public static final double kBackRightModulelAngularOffset = 2.109223;
 
     public static final double kHeadingCorrectionP = 0.1;
     public static final double kHeadingCorrectionTolerance = 1.0;
