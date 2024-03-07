a02fcd0d4500e4ec5bb2cd2dbc77c4e13c3a947c
SeanLi1234 <sli-25@peddie.org>
2024-03-07 16:47:52 -0500

    Update Intake.java

--- 2024-I/src/main/java/frc/robot/subsystems/Intake.java
+++ 2024-I/src/main/java/frc/robot/subsystems/Intake.java
@@ -74,6 +74,7 @@ public class Intake extends SubsystemBase {
     return intakeMotor.getSupplyCurrent();
   }
 
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
