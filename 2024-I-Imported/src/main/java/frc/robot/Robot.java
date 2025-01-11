174e110547f6efbf1ed8a9be872127869cf51ff2
kaseyLee123 <142427883+kaseyLee123@users.noreply.github.com>
2025-01-11 12:33:51 -0500

    elevator

---
+++ 2024-I-Imported/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,60 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.Elevator;
+
+/** This is a sample program to demonstrate the use of elevator simulation. */
+public class Robot extends TimedRobot {
+  //private final Joystick m_joystick = new Joystick(Constants.kJoystickPort);
+  private final PS4Controller controller;
+  private final Elevator m_elevator = new Elevator();
+  private final Trigger oButton;
+
+  public Robot() {
+    controller = new PS4Controller(0);
+    oButton = new JoystickButton(controller, PS4Controller.Button.kCircle.value);
+  }
+
+  @Override
+  public void robotPeriodic() {
+    // Update the telemetry, including mechanism visualization, regardless of mode.
+    m_elevator.updateTelemetry();
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // Update the simulation model.
+    m_elevator.simulationPeriodic();
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    if (oButton.getAsBoolean()) {
+      // Here, we set the constant setpoint of 0.75 meters.
+      //m_elevator.reachGoal(Constants.kSetpointMeters);
+    } else {
+      // Otherwise, we update the setpoint to 0.
+      //m_elevator.reachGoal(0.0);
+    }
+  }
+
+  @Override
+  public void disabledInit() {
+    // This just makes sure that our simulation code knows that the motor's off.
+    m_elevator.stop();
+  }
+
+  @Override
+  public void close() {
+    m_elevator.close();
+    super.close();
+  }
+}
